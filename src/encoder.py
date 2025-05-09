d7053ae8d1725153aee5f3c1f371084e67fbf791
Theodor Isacsson <theodor@isacsson.ca>
2025-05-09 11:18:56 -0700

    Add template for src

---
+++ src/encoder.py
@@ -0,0 +1,10 @@
+import torch
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, n_latents: int = 256):
+        super().__init__()
+        ...
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pass
